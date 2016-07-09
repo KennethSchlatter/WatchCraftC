5aa84304cae831190b8e4cb5698cef489f0225f9
kenneth.schlatter@gmail.com <kenneth.schlatter@gmail.com>
2016-07-09 13:31:50 -0400

    Added NetDownload libraries

--- src/craft.h
+++ src/craft.h
@@ -1,1 +1,6 @@
 #pragma once
+#include "main.h"
+void craft_window_init();
+void craft_window_deinit();
+void craft_window_set_recipe(Recipe recipe);
+Window *craft_window_get_window();
--- src/main.h
+++ src/main.h
@@ -1,1 +1,8 @@
 #pragma once
+
+typedef struct Recipe {
+    char title[1][14];
+    char name[1][20];
+    char recipe_url[1][128];
+    uint8_t type;
+} Recipe;
